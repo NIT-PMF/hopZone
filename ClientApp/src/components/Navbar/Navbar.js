296ac626540bdcbbb2c3c6eb55ac1acd3ad3580a
tarik11 <60644723+tarik11@users.noreply.github.com>
2020-04-05 09:23:19 +0200

    ClickLogo

--- ClientApp/src/components/Navbar/Navbar.js
+++ ClientApp/src/components/Navbar/Navbar.js
@@ -3,13 +3,14 @@ import Logo from '../Logo/Logo';
 import LinkNavbar from '../LinkNavbar/LinkNavbar';
 import logo from '../../assets/images/logoHopzone.svg';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <div className="navigation-component">
             <ol className="nav-list">
                 <li className="nav-item-logo">
-                    <Logo width={'15%'} />
+                    <Link to="/"><Logo width={'25%'} /> </Link>
                 </li>
                 <LinkNavbar path="/">Home</LinkNavbar>
                 <LinkNavbar path="/about">About</LinkNavbar>
