9c1b1282d5ce8a7cc28ab03a400354f35ffb375c
Irhad Fejzic <irhad.fejzic@gmail.com>
2020-04-16 16:23:19 +0200

    before admin

--- ClientApp/src/components/Account/Login/Login.js
+++ ClientApp/src/components/Account/Login/Login.js
@@ -7,39 +7,40 @@ const LoginForm = () => {
 
     return (
         <React.Fragment>
-            <div className="App__Aside">Lets get you set up!
-                <span className="App__Aside_Small">
-                    It should only take a couple of minutes to create your account. <br />
-                        You can add additional info about yourself in the profile page :)
-                        </span>
-            </div>
-            <div className="App__Form">
-                <div className="PageSwitcher">
-                    <NavLink to="/account/login" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Login</NavLink>
-                    <NavLink exact to="/account/signup" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
+            <div className="login-container">
+                <div className="App__Aside">Welcome Back!
+                <div className="App__Aside_Small">
+                        Type in your information to the right and you will be redirected to your profile page.
+                        </div>
                 </div>
+                <div className="App__Form">
+                    <div className="PageSwitcher">
+                        <NavLink to="/account/login" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Login</NavLink>
+                        <NavLink exact to="/account/signup" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
+                    </div>
 
-                {/*
+                    {/*
                 <div className="FormTitle">
                     <NavLink to="/account/login" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Login</NavLink> or <NavLink exact to="/account/signup" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
                </div>
                 */}
-                <div className="FormCenter">
-                    <form className="FormFields">
-                        <div className="FormField">
-                            <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
-                            <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" />
-                        </div>
+                    <div className="FormCenter">
+                        <form className="FormFields">
+                            <div className="FormField">
+                                <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
+                                <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" />
+                            </div>
 
-                        <div className="FormField">
-                            <label className="FormField__Label" htmlFor="password">Password</label>
-                            <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password" />
-                        </div>
+                            <div className="FormField">
+                                <label className="FormField__Label" htmlFor="password">Password</label>
+                                <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password" />
+                            </div>
 
-                        <div className="FormField">
-                            <button className="FormField__Button mr-20"> Login </button>
-                        </div>
-                    </form>
+                            <div className="FormField">
+                                <button className="FormField__Button mr-20"> Login </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </React.Fragment>
--- ClientApp/src/components/Account/Signup/Signup.js
+++ ClientApp/src/components/Account/Signup/Signup.js
@@ -9,44 +9,63 @@ const SignupForm = () => {
 
     return (
         <React.Fragment>
-            <div className="App__Aside">Lets get you set up!
-                <span className="App__Aside_Small">
-                    It should only take a couple of minutes to create your account. <br />
-                        You can add additional info about yourself in the profile page :)
-                        </span>
-            </div>
-            <div className="App__Form">
-                <div className="PageSwitcher">
-                    <NavLink to="/account/login" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Login</NavLink>
-                    <NavLink exact to="/account/signup" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
+            <div className="signup-container">
+                <div className="App__Aside">Lets get you set up!
+                <div className="App__Aside_Small">
+                        It should only take a couple of minutes to create your account. <br />
+                        You can add additional info about yourself later in the profile page :)
+                        </div>
                 </div>
+                <div className="App__Form">
+                    <div className="PageSwitcher">
+                        <NavLink to="/account/login" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Login</NavLink>
+                        <NavLink exact to="/account/signup" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
+                    </div>
 
 
-                <div className="FormCenter">
-                    <form className="FormFields">
-                        <div className="FormField">
-                            <label className="FormField__Label" htmlFor="name">Full Name</label>
-                            <input type="text" id="name" className="FormField__Input" placeholder="Enter your full name" name="name" />
-                        </div>
-                        <div className="FormField">
-                            <label className="FormField__Label" htmlFor="password">Password</label>
-                            <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password" />
-                        </div>
-                        <div className="FormField">
-                            <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
-                            <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" />
-                        </div>
+                    <div className="FormCenter">
+                        <form className="FormFields">
+                            <div className="FormField">
+                                <label className="FormField__Label" htmlFor="name">Username</label>
+                                <input type="text" className="FormField__Input" placeholder="Enter your username" />
+                            </div>
+                            <div className="FormField Field_first_last_name">
+                                <div className="Form_Name">
+                                <label className="FormField__Label" htmlFor="name">First Name</label>
+                                    <input type="text" className="FormField__Input" placeholder="Enter your first name" />
+                                </div>
+                                <div className="Form_Name">
+                                    <label className="FormField__Label" htmlFor="name">Last Name</label>
+                                    <input type="text" className="FormField__Input" placeholder="Enter your last name" />
+                                </div>
+                            </div>
+                            <div className="FormField">
+                                <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
+                                <input type="email" className="FormField__Input" placeholder="Enter your email" name="email" />
+                            </div>
+                            <div className="FormField">
+                                <label className="FormField__Label" htmlFor="password">Password</label>
+                                <input type="password" className="FormField__Input" placeholder="Enter your password" name="password" />
+                            </div>
+                            <div className="FormField">
+                                <label className="FormField__Label" htmlFor="password">Repeat Password</label>
+                                <input type="password" className="FormField__Input" placeholder="Repeat your password" name="password" />
+                            </div>
 
-                        <div className="FormField">
-                            <label className="FormField__CheckboxLabel">
-                                <input className="FormField__Checkbox" type="checkbox" name="hasAgreed" /> I agree all statements in <a href="" className="FormField__TermsLink">terms of service</a>
-                            </label>
-                        </div>
+                            <div className="FormField">
+                                <div className="FormField__CheckboxLabel">
+                                    <input className="FormField__Checkbox" type="checkbox" name="hasAgreed" />
+                                    <div className="confirm-checkbox">
+                                        I agree all statements in <a href="" className="FormField__TermsLink">terms of service</a>
+                                    </div>
+                                </div>
+                            </div>
 
-                        <div className="FormField">
-                            <button className="FormField__Button mr-20">Sign Up</button>
-                        </div>
-                    </form>
+                            <div className="FormField">
+                                <button className="FormField__Button mr-20">Sign Up</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </React.Fragment>
