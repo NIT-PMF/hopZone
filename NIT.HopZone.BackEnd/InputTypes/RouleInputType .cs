65191056b63a99bd194d73a2c3374bffcb9ee36f
tarik11 <60644723+tarik11@users.noreply.github.com>
2020-06-15 07:41:56 +0200

    push

---
+++ NIT.HopZone.BackEnd/InputTypes/RouleInputType .cs
@@ -0,0 +1,19 @@
+﻿
+using GraphQL.Types;
+using NIT.HopZone.Web.Models;
+
+namespace NIT.HopZone.BackEnd.InputTypes
+{
+    public class RouteInputType : InputObjectGraphType // promjenili
+    {
+        public RouteInputType()
+        {
+            Name = "RouteInputType";
+
+            Field<StringGraphType>("Name");
+            Field<StringGraphType>("Description");
+            Field<IntGraphType>("Id_city");
+            Field<StringGraphType>("ImageURL");
+        }
+    }
+}
---
+++ NIT.HopZone.BackEnd/InputTypes/SpotInputType.cs
@@ -0,0 +1,26 @@
+﻿
+using GraphQL.Types;
+using NIT.HopZone.Web.Models;
+
+namespace NIT.HopZone.BackEnd.InputTypes
+{
+    public class SpotInputType : InputObjectGraphType // promjenili
+    {
+        public SpotInputType()
+        {
+            Name = "SpotInputType";
+
+            Field<StringGraphType>("Description");
+
+            Field<StringGraphType>("Id_map_coordinate");
+
+            Field<StringGraphType>("ImageURL");
+
+            Field<StringGraphType>("Id_city");
+
+            Field<StringGraphType>("logitude");
+
+            Field<StringGraphType>("latitude");
+        }
+    }
+}
